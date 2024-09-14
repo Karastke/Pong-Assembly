a612e1dd5a137d94d0b20946a510c963b77c8f25
Karastke <fkdlsaos1@naver.com>
2024-09-14 14:41:18 +0900

    세그먼트 레지스터 설정 및 스택 초기화

--- Pong.asm
+++ Pong.asm
@@ -2,13 +2,24 @@ STACK SEGMENT PARA STACK
     DB 64 DUP (' ')
 STACK ENDS
 
-DATA SEGMENT PARA 'DATA'
+DATA SEGMENT PARA 'DATA' ;데이터를 저장하는 segment
+
+    BALL_X DW 0Ah
+    BALL_Y DW 0Ah
 
 DATA ENDS
 
 CODE SEGMENT PARA 'CODE'
 
     MAIN PROC FAR
+    ASSUME CS:CODE,DS:DATA,SS:STACK ;assume as code,data, stack segment the respective registers
+    PUSH DS                         ;push to the stack the DS segment
+    SUB AX,AX                       ;clean the AX register
+    PUSH AX                         ;push AX to the stack
+    MOV AX,DATA                     ;save on the AX register the contents of the DATA segment
+    MOV DS,AX                       ;save on the DS segment the contents of AX
+    POP AX                          ;release the top item from the stack to the AX register
+    POP AX
 
         MOV AH,00h ;set the configuration to video mode
         MOV AL,13h ;choose the video mode
@@ -22,8 +33,8 @@ CODE SEGMENT PARA 'CODE'
         MOV AH, 0Ch ;set the configuration to writing a pixel
         MOV AL, 0Fh ;choose white as color
         MOV BH, 00h ;set the page number
-        MOV CX, 0Ah ;set the column (X 축)
-        MOV DX, 0Ah ;set the line (Y 축)
+        MOV CX, BALL_X ;set the column (X 축)
+        MOV DX, BALL_Y ;set the line (Y 축)
         INT 10h     ;execute the configuration
 
         RET
